2363a8ccd3fcd987b74161a5cb9c5e60cc4bef57
Hycaro Phillype <phillypehycaro@gmail.com>
2025-01-13 16:06:04 -0300

    feat: Get usuarios
    Só pra testar, depois tenho que deletar

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
 export class UsuarioController {
   private usuarioRepository = new UsuarioRepository();
+
+  @Get()
+  async listUsuario(@Get() mostrarUsuarios) {
+    return mostrarUsuarios;
+  }
+
   @Post()
   async criaUsuario(@Body() dadosUsuario) {
     this.usuarioRepository.salvar(dadosUsuario);

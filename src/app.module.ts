b84a56ce2569e850013c01e2ecc0e3f09854587c
Hycaro Phillype <phillypehycaro@gmail.com>
2025-01-21 10:36:22 -0300

    Refatorando as configurações

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
+import * as crypto from 'crypto';
 
+(global as any).crypto = crypto;
 @Module({
   imports: [
     UsuarioModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
-      inject: [PostgresConfigService]
-    })
+      inject: [PostgresConfigService],
+    }),
   ],
 })
 export class AppModule {}

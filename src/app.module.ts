5b857becf2cb666d07aeea3da4e8ecee8ca5f4b8
Hycaro Phillype <phillypehycaro@gmail.com>
2025-01-21 09:37:13 -0300

    Preparando ambiente para conctar ao banco de dados(configurando TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/db.config.service';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}

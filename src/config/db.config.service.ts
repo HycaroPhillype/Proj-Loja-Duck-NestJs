3a867464cb8ae9eb7b0d442f40c8163de91d2a63
Hycaro Phillype <phillypehycaro@gmail.com>
2025-01-22 15:12:49 -0300

    Criando a primeira entidade

--- src/config/db.config.service.ts
+++ src/config/db.config.service.ts
@@ -8,11 +8,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: this.configService.get<string>('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_USERNAME'),
-      password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_NAME'),
+      url:this.configService.get<string>('DB_URL'),
       entities: [],
       synchronize: true
 
--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,11 +1,27 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn } from 'typeorm';
 
-
+@Entity({ name: 'users'})
 export class UserEntity {
   splice(index: number, arg1: number) {
     throw new Error('Method not implemented.');
   }
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column({ name: 'nome', length: 100, nullable: false })
   nome: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
   email: string;
+
+  @Column({ name: 'senha', length: 255, nullable: false })
   senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  cratedAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }

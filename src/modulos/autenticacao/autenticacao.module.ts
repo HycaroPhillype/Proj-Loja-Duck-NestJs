9b6dc126f3c446870d85546b3ac5fc44c459932d
Hycaro Phillype <phillypehycaro@gmail.com>
2025-02-24 14:56:44 -0300

    feat: Criando e retornando um JWT

--- src/modulos/autenticacao/autenticacao.module.ts
+++ src/modulos/autenticacao/autenticacao.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './autenticacao.service';
 import { AuthenticationController } from './autenticacao.controller';
 import { UserModule } from '../usuario/usuario.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'SEGREDO_SECRETO',
+      signOptions: { expiresIn: '72h'},
+    })
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
--- src/modulos/autenticacao/autenticacao.service.ts
+++ src/modulos/autenticacao/autenticacao.service.ts
@@ -1,23 +1,36 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../usuario/usuario.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+interface UsuerPayload {
+  sub: string;
+  nameUser: string;
+}
 @Injectable()
 export class AuthenticationService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(email: string, senhaInserted: string) {
     const user = await this.userService.searchByEmail(email);
 
-    const userAuthentic = await bcrypt.compare(
-      senhaInserted,
-      user.senha,
-    );
+    const userAuthentic = await bcrypt.compare(senhaInserted, user.senha);
 
     if (!userAuthentic) {
       throw new UnauthorizedException('O email ou a senha está incorreto.');
     }
 
-    console.log('Usuário autenticado');
+    const payload: UsuerPayload = {
+      sub: user.id,
+      nameUser: user.nome
+    };
+
+    return {
+      token_access: await this.jwtService.signAsync(payload),
+    }
 
   }
 }

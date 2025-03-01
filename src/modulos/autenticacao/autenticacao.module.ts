db7d8defd0825b9806bfd7e6fb1f072de0aa8837
Hycaro Phillype <phillypehycaro@gmail.com>
2025-03-01 09:37:52 -0300

    feat: Configurando variáveis de ambiete

--- src/modulos/autenticacao/autenticacao.module.ts
+++ src/modulos/autenticacao/autenticacao.module.ts
@@ -3,13 +3,19 @@ import { AuthenticationService } from './autenticacao.service';
 import { AuthenticationController } from './autenticacao.controller';
 import { UserModule } from '../usuario/usuario.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return{
+          secret: configService.get<string>('SEGREDO_JWT'),
+          signOptions: { expiresIn: '72h'},
+        }
+      },
+      inject: [ConfigService],
       global: true,
-      secret: 'SEGREDO_SECRETO',
-      signOptions: { expiresIn: '72h'},
     })
   ],
   controllers: [AuthenticationController],
